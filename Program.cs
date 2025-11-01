b9083825a87ac46d0ddfb9b7e634096b767cf061
SNavada6801 <snavada@lnw.com>
2025-11-01 09:24:11 +0530

    Updated the program.cs
    Created an instance for every denom and caleed the method to rename the math files.

--- Program.cs
+++ Program.cs
@@ -13,6 +13,21 @@ namespace Math_Naming_Convention
             FileHandler fd1 = new FileHandler(1);
             fd1.MoveMathFilesToNewRenamedFile(fnd.directoryOfDenoms[0].TrimEnd("/new_math_name.txt".ToCharArray()),fd1.dict1);
 
+            FileHandler fd2 = new FileHandler(2);
+            fd2.MoveMathFilesToNewRenamedFile(fnd.directoryOfDenoms[1].TrimEnd("/new_math_name.txt".ToCharArray()), fd2.dict2);
+
+            FileHandler fd5 = new FileHandler(5);
+            fd5.MoveMathFilesToNewRenamedFile(fnd.directoryOfDenoms[2].TrimEnd("/new_math_name.txt".ToCharArray()), fd5.dict5);
+
+            FileHandler fd10 = new FileHandler(10);
+            fd10.MoveMathFilesToNewRenamedFile(fnd.directoryOfDenoms[3].TrimEnd("/new_math_name.txt".ToCharArray()), fd10.dict10);
+
+            FileHandler fd100 = new FileHandler(100);
+            fd100.MoveMathFilesToNewRenamedFile(fnd.directoryOfDenoms[4].TrimEnd("/new_math_name.txt".ToCharArray()), fd100.dict100);
+
+            FileHandler fd200 = new FileHandler(200);
+            fd200.MoveMathFilesToNewRenamedFile(fnd.directoryOfDenoms[5].TrimEnd("/new_math_name.txt".ToCharArray()), fd200.dict200);
+
             //FileHandler fd1 = new FileHandler(1);
             //fd1.PerFormMathNamingConventionAndPrintInFile(1);
             //fd1.CountUniquePercentsInAllMath(1);
